9c78cda2a5ae3c0195d5f9e43f158449ed97765c
nihargupte2008@gmail.com <31829255+iamNihar07@users.noreply.github.com>
2020-10-20 07:26:55 -0500

    added dataset read

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-import numpy as np
-import pandas as pd
+import numpy
+import pandas
 import itertools
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 
+#Read the data
+dataset=pandas.read_csv('dataset\\news.csv')
+
+#Display the nummber of rows and columns of the dataset
+print(dataset.shape)
+
+#Preview the first five tuples of the dataset
+print(dataset.head())
